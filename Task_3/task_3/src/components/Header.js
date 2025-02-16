b12bba3c0f07cdc1bb966a7c088bf3eccec02768
Pavlo Stovba <stovbapavlo@gmail.com>
2025-02-16 14:14:44 +0200

    Task_3: Finished header implementation

---
+++ Task_3/task_3/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>FLAT UI COLORS 2</h1>
+      <nav className={styles.nav}>
+        <Link to="#" className={styles.link}>
+          Designer Inspiration
+        </Link>
+        <Link to="#" className={styles.link}>
+          Subscribe
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
