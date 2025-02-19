f03e9e2b905000388a53ff8ac2b4734739e83aaf
Pavlo Stovba <stovbapavlo@gmail.com>
2025-02-19 18:32:26 +0200

    Task_4: Initialize project, add Header and Footer components

---
+++ Task_4/task_4/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// import Categories from './pages/Categories';
+// import BlogDetails from './pages/BlogDetails';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './styles/global.scss';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/categories" element={<Categories />} />
+          <Route path="/blog/:id" element={<BlogDetails />} /> */}
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
