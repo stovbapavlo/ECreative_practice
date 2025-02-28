911d4dab8d55dcdc400cf10e9685856cfadd4c8c
Pavlo Stovba <stovbapavlo@gmail.com>
2025-02-28 23:19:34 +0200

    Task_5: executed the main elements and stylized them

--- Task_5/task_5/src/App.tsx
+++ Task_5/task_5/src/App.tsx
@@ -1,20 +1,41 @@
 import RegistrationForm from './pages/RegistrationForm';
 import ProfileInfoForm from './pages/ProfileInfoForm';
 
-import { createContext, useState } from 'react';
+import { createContext, useState, useMemo, useCallback } from 'react';
 
-export const FormContext = createContext({});
+export const FormContext = createContext({
+  typedPhone: '',
+  setTypedPhone: (phone: string) => {},
+  email: '',
+  setEmail: (email: string) => {},
+});
 
 function App() {
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({});
+  const [typedPhone, setTypedPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [selectedCode, setSelectedCode] = useState('+1');
+
+  const formContextValue = useMemo(
+    () => ({ typedPhone, setTypedPhone, email, setEmail, selectedCode, setSelectedCode }),
+    [typedPhone, email, selectedCode],
+  );
+
+  const handleNextStep = useCallback(() => setStep((prev) => prev + 1), []);
 
   return (
-    <FormContext.Provider value={{ formData, setFormData }}>
+    <FormContext.Provider value={formContextValue}>
       <div className="app-container">
-        {/* <ProfileInfoForm onNext={() => setStep(3)} /> */}
-        {step === 1 && <RegistrationForm onNext={() => setStep(2)} />}
-        {step === 2 && <ProfileInfoForm onNext={() => setStep(3)} />}
+        {step === 1 && <RegistrationForm onNext={handleNextStep} onClose={() => {}} />}
+        {step === 2 && (
+          <ProfileInfoForm
+            onNext={handleNextStep}
+            typedPhone={typedPhone}
+            email={email}
+            selectedCode={selectedCode}
+          />
+        )}
       </div>
     </FormContext.Provider>
   );
